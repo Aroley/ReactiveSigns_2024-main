9f3916384d279c43488fee2bd78a8edbe7a87938
Aline Wyss <aline.wyss@zhdk.ch>
2024-12-03 17:01:18 +0100

    Spikey exists

--- projects/01MainAssignments/project1/script.js
+++ projects/01MainAssignments/project1/script.js
@@ -8,6 +8,7 @@ function preload() {
 }
 
 function setup() {
+    createCanvas(windowWidth, windowHeight)
     // create grid array
     for (let i = 0; i < gridX; i++) {
         for (let j = 0; j < gridY; j++) {
@@ -23,11 +24,18 @@ function setup() {
 
 function draw() {
 
+    let s1 = new Spikey(width / 2, height / 2, 2)
+    s1.display()
+
+    /* let s2 = new Spikey (400,200,5)
+    s2.display()
+         */
+
     // draw grid
-    for (let i = 0; i < tileArray.length; i++) {
+    /*for (let i = 0; i < tileArray.length; i++) {
         tileArray[i].display()
         console.log(tileArray[i])
-    }
+    }*/
 }
 
 // grid tiles
@@ -49,4 +57,43 @@ class Tile {
         }
         rect(this.x, this.y, this.size, this.size)
     }
+}
+// Spikey
+class Spikey {
+    constructor(x, y, size, done) {
+        this.x = x
+        this.y = y
+        this.size = size
+        this.done = done
+    }
+
+    display() {
+        // translate(width/2,height/2)
+        fill(0)
+        stroke(0)
+        strokeWeight(0.1)
+        ellipse(this.x, this.y, this.size)
+        //rotate(frameCount*0.005);
+        for (let angle = 0; angle <= 360; angle += 1) {
+            // Save current coordinate system                      
+            push()
+            translate(this.x, this.y);
+            rotate(radians(angle));
+            /* let lastVar = randomGen(angle) */
+            line(0, 0, random(10, 500), 0); // add last var
+            pop()
+        }
+    }
+
+  /*   randomGen(times) {
+        let number = 0
+        if (!done) {
+            number = random(10, 500)
+            if(times<360){
+            done = true  
+            }
+            
+        }
+        return number
+    } */
 }
