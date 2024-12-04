98d59414833a7409e83396707844e64295a85460
Aroley <47256141+Aroley@users.noreply.github.com>
2024-12-04 16:43:50 +0100

    Digit 2 added

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
+/* Reminder to set spikey 1,3,5 to left and spikey 2,4,6 to right */
+
 let gridX = 6;
 let gridY = 7;
 let tileArray = [];
-let spikey;
+let spikey1;
 let spikey2;
 let spikey3;
 let spikey4;
 let spikey5;
 let spikey6;
+let one;
+let two;
+let three;
+let four;
+let five;
+let six;
+let seven;
+let eight;
+let nine;
+let zero;
 let numbers = [];
 let mousePos
 let tileSize = 100
@@ -18,81 +30,122 @@ function preload() {
 
 function setup() {
     createCanvas(600, 700);
-    rectMode(CORNER) 
-     // spikeys
-    spikey = new Spikey(width / 2, height / 2, 400);
-    spikey2 = new Spikey(width / 2, height / 2, 400);
-    spikey3 = new Spikey(width / 2, height / 2, 400);
-/*     spikey4 = new Spikey(width / 2, height / 2, 400);
-    spikey5 = new Spikey(width / 2, height / 2, 400);
-    spikey6 = new Spikey(width / 2, height / 2, 400); */
+    rectMode(CORNER)
+    // Spikeys
+    spikey1 = new Spikey(width / 2, height / 2, 220);
+    spikey2 = new Spikey(width / 2, height / 2, 220);
+    spikey3 = new Spikey(width / 2, height / 2, 220);
+    spikey4 = new Spikey(width / 2, height / 2, 220);
+    spikey5 = new Spikey(width / 2, height / 2, 220);
+    spikey6 = new Spikey(width / 2, height / 2, 220);
 
     // create grid
     for (let i = 0; i < gridX; i++) {
         for (let j = 0; j < gridY; j++) {
             let showTile = false;
-            
-            let tile = new Tile(i * tileSize, j * tileSize, 100, showTile,i * gridY + j);
+
+            let tile = new Tile(i * tileSize, j * tileSize, 100, showTile, i * gridY + j);
             tileArray.push(tile);
         }
     }
     // set up numbers
-    let one = [false,false,false,false,false,false,
-            false,false,false,false,false,false,
-            false,false,true,false,false,false,
-            false,false,false,true,true,true,
-            true,true,true,true,false,false,false,
-            false,false,false,false,false,false,
-            false,false,false,false,false,false]
+     one = [false, false, false, false, false, false,
+        false, false, false, false, false, false,
+        false, false, true, false, false, false,
+        false, false, false, true, true, true,
+        true, true, true, true, false, false,
+        false, false, false, false, false, false,
+        false, false, false, false, false, false]
     numbers.push(one)
 
-   
+     two = [true, false, false, true, true, true,
+        true, true, false, false, true, false,
+        false, true, true, false, false, true,
+        false, false, true, true, false, false,
+        true, false, false, true, true, false,
+        false, true, false, false, true, true,
+        true, true, true, false, false, true]
+    numbers.push(two)
+
+
 }
 
 function draw() {
     background(255);
-    // number grid
-    showTemplate(numbers[0])
+    // choose number grid to display
+    showTemplate(numbers[1])
 
-    
-    spikey.display();
+
+    spikey1.display();
     spikey2.display();
     spikey3.display();
-    
+    spikey4.display();
+    spikey5.display();
+    spikey6.display();
+
 
     // draw grid
     for (let i = 0; i < gridX; i++) {
         for (let j = 0; j < gridY; j++) {
             let index = i * gridY + j;
-            
+
             tileArray[index].display();
         }
-    } 
-   
-}
-
-function mouseClicked() {
-    tileArray.forEach(tile => {
-            
-        
-    });
+    }
 
 }
 
-function showTemplate(template){
+function showTemplate(template) {
     for (let i = 0; i < tileArray.length; i++) {
         // get true or false value from the digit template and apply to Tiles
         tileArray[i].show = template[i]
     }
+
     // adjust the spikeys' position
-    spikey.x=tileArray[14].x
-    spikey.y=tileArray[14].y
+    switch (template) {
+        case one:
 
-    spikey2.x=tileArray[28].x
-    spikey2.y=tileArray[28].y
+            spikey1.x = tileArray[14].x
+            spikey1.y = tileArray[14].y
 
-    spikey3.x=tileArray[34].x
-    spikey3.y=tileArray[34].y+tileSize
+            spikey2.x = tileArray[21].x+ tileSize
+            spikey2.y = tileArray[21].y
+
+            spikey3.x = tileArray[34].x
+            spikey3.y = tileArray[34].y + tileSize
+
+            spikey4.x = -width
+            spikey4.y = tileArray[3].y
+
+            spikey5.x = -width
+            spikey5.y = tileArray[3].y
+
+            spikey6.x = -width
+            spikey6.y = tileArray[3].y
+
+            break;
+
+        case two:
+            spikey1.x = tileArray[0].x
+            spikey1.y = tileArray[0].y
+
+            spikey2.x = tileArray[35].x + tileSize
+            spikey2.y = tileArray[35].y
+
+            spikey3.x = tileArray[3].x
+            spikey3.y = tileArray[3].y
+
+            spikey4.x = tileArray[38].x + tileSize
+            spikey4.y = tileArray[38].y + tileSize
+
+            spikey5.x = tileArray[6].x
+            spikey5.y = tileArray[6].y + tileSize
+
+            spikey6.x = tileArray[41].x + tileSize
+            spikey6.y = tileArray[41].y + tileSize
+
+    }
+
 
 
 }
@@ -104,7 +157,7 @@ class Tile {
         this.y = y;
         this.size = size;
         this.show = show;
-        this.id = id+1
+        this.id = id + 1
     }
 
     display() {
@@ -123,7 +176,7 @@ class Tile {
         rect(this.x, this.y, this.size, this.size);
         fill(0)
         textAlign(CENTER)
-       // text(this.id,this.x+50,this.y+50)
+        text(this.id, this.x + 50, this.y + 50)
     }
 }
 
@@ -133,18 +186,19 @@ class Spikey {
         this.y = y;
         this.size = size;
         this.lines = [];
-        
+
 
         // generate the lines once
+        // TODO: only show maybe 90 degres?
         for (let angle = 0; angle <= 360; angle += 1) {
             let length = random(10, this.size);
             this.lines.push({ angle, length });
-            
-        } 
+
+        }
     }
 
     display() {
-        mousePos = map(mouseX,0,width,0.005,0.06)
+        mousePos = map(mouseX, 0, width, 0.005, 0.06)
         fill(0);
         stroke(0);
         strokeWeight(0.6);
@@ -154,13 +208,13 @@ class Spikey {
             push();
             translate(this.x, this.y);
             rotate(radians(l.angle));
-            let oscillatingLength =0
-            if(l.length>this.size*0.7){
-            oscillatingLength = this.sinMovement(l.angle, frameCount * mousePos, this.size*0.45, this.size);
-            }else {
-            oscillatingLength = this.sinMovement(l.angle, frameCount * mousePos, this.size*0.2, this.size*0.7);
+            let oscillatingLength = 0
+            if (l.length > this.size * 0.7) {
+                oscillatingLength = this.sinMovement(l.angle, frameCount * mousePos, this.size * 0.45, this.size);
+            } else {
+                oscillatingLength = this.sinMovement(l.angle, frameCount * mousePos, this.size * 0.2, this.size * 0.7);
             }
-            
+
             line(0, 0, oscillatingLength, 0);
             pop();
         }
