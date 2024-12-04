87b0569c55959df1eba0a8444baf41dff3f19fc6
Aroley <47256141+Aroley@users.noreply.github.com>
2024-12-04 15:29:38 +0100

    Working digit one

--- script.js
+++ script.js
@@ -2,7 +2,14 @@ let gridX = 6;
 let gridY = 7;
 let tileArray = [];
 let spikey;
+let spikey2;
+let spikey3;
+let spikey4;
+let spikey5;
+let spikey6;
 let numbers = [];
+let mousePos
+let tileSize = 100
 
 function preload() {
     font = loadFont('Montserrat-Black.otf');
@@ -13,16 +20,20 @@ function setup() {
     createCanvas(600, 700);
     rectMode(CORNER) 
      // spikeys
-    spikey = new Spikey(width / 2, height / 2, 10);
+    spikey = new Spikey(width / 2, height / 2, 400);
+    spikey2 = new Spikey(width / 2, height / 2, 400);
+    spikey3 = new Spikey(width / 2, height / 2, 400);
+/*     spikey4 = new Spikey(width / 2, height / 2, 400);
+    spikey5 = new Spikey(width / 2, height / 2, 400);
+    spikey6 = new Spikey(width / 2, height / 2, 400); */
 
     // create grid
     for (let i = 0; i < gridX; i++) {
         for (let j = 0; j < gridY; j++) {
             let showTile = false;
             
-            let tile = new Tile(i * 100, j * 100, 100, showTile,i * gridY + j);
+            let tile = new Tile(i * tileSize, j * tileSize, 100, showTile,i * gridY + j);
             tileArray.push(tile);
-            console.log("logged " + tile.x);
         }
     }
     // set up numbers
@@ -38,18 +49,15 @@ function setup() {
    
 }
 
-// Clear the canvas
 function draw() {
     background(255);
-
-    // form grid
+    // number grid
     showTemplate(numbers[0])
 
-   
     
     spikey.display();
-
-
+    spikey2.display();
+    spikey3.display();
     
 
     // draw grid
@@ -63,14 +71,30 @@ function draw() {
    
 }
 
+function mouseClicked() {
+    tileArray.forEach(tile => {
+            
+        
+    });
+
+}
+
 function showTemplate(template){
     for (let i = 0; i < tileArray.length; i++) {
         // get true or false value from the digit template and apply to Tiles
         tileArray[i].show = template[i]
     }
-    // adjust the spikeys
+    // adjust the spikeys' position
     spikey.x=tileArray[14].x
     spikey.y=tileArray[14].y
+
+    spikey2.x=tileArray[28].x
+    spikey2.y=tileArray[28].y
+
+    spikey3.x=tileArray[34].x
+    spikey3.y=tileArray[34].y+tileSize
+
+
 }
 
 // grid tiles
@@ -84,60 +108,70 @@ class Tile {
     }
 
     display() {
+        noStroke()
         if (this.show) {
-            noFill();
-            stroke(200, 20, 20);
+            // Debug aid
+            /* noFill();
+            stroke(200, 20, 20); */
+            noFill()
         } else {
-            fill(200, 20, 20);
-            stroke(0);
+            // Debug aid
+            fill(255)
+            /* fill(200, 20, 20);
+            stroke(0); */
         }
         rect(this.x, this.y, this.size, this.size);
         fill(0)
         textAlign(CENTER)
-        text(this.id,this.x+50,this.y+50)
+       // text(this.id,this.x+50,this.y+50)
     }
 }
 
-// Spikey
 class Spikey {
     constructor(x, y, size) {
         this.x = x;
         this.y = y;
         this.size = size;
-        this.lines = []; // Store the lines to be drawn
+        this.lines = [];
+        
 
-        // Generate the lines once
-        for (let angle = 0; angle <= 360; angle += 2) {
-            let length = random(10, 300);
+        // generate the lines once
+        for (let angle = 0; angle <= 360; angle += 1) {
+            let length = random(10, this.size);
             this.lines.push({ angle, length });
-        }
+            
+        } 
     }
 
     display() {
+        mousePos = map(mouseX,0,width,0.005,0.06)
         fill(0);
         stroke(0);
-        strokeWeight(0.2);
-        ellipse(this.x, this.y, this.size);
+        strokeWeight(0.6);
+
+        // apply lines to Spikey
         for (let l of this.lines) {
             push();
             translate(this.x, this.y);
             rotate(radians(l.angle));
-            line(0, 0, l.length, 0);
+            let oscillatingLength =0
+            if(l.length>this.size*0.7){
+            oscillatingLength = this.sinMovement(l.angle, frameCount * mousePos, this.size*0.45, this.size);
+            }else {
+            oscillatingLength = this.sinMovement(l.angle, frameCount * mousePos, this.size*0.2, this.size*0.7);
+            }
+            
+            line(0, 0, oscillatingLength, 0);
             pop();
         }
     }
 
-
-    wiggle() {
-        for (let line of this.lines) {
-            line.length = this.sinMovement(line.angle, 0.1, 10, 500);
-        }
-    }
-
     sinMovement(angle, offset, minVal, maxVal) {
-        let factor = sin(angle + offset)
-        let sinMovementVal = map(factor, -1, 1, minVal, maxVal)
+        let factor = sin(angle + offset);
+        let sinMovementVal = map(factor, -1, 1, minVal, maxVal);
         return sinMovementVal;
     }
+
+
 
 }
